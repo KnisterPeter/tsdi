e98687900aa0a36a5a6b45bf704082d468709f58
Markus Wolf <mail@markus-wolf.de>
2016-02-17 07:35:36 +0100

    fix: Finished typescript packaging for npm

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,25 @@
-declare module 'tsdi' {
-
-  export type Constructable<T> = { new(): T; };
-
-  export interface IComponentOptions {
+export declare type Constructable<T> = {
+    new (): T;
+};
+export interface IComponentOptions {
     name?: string;
-  }
-
-  export interface IInjectOptions {
-    name?: string;
-  }
-
-  export class TSDI {
-    public addProperty(key: string, value: any): void;
-    public close(): void;
-    public enableComponentScanner(): void;
-    public register(component: Constructable<any>, name?: string): void;
-    public get<T>(component: Constructable<T>, hint?: string): T;
-
-  }
-
-  export function Component(options?: IComponentOptions): ClassDecorator;
-  export function Inject(options?: IInjectOptions): PropertyDecorator;
-  export function Initialize(): MethodDecorator;
-
 }
+export interface IInjectOptions {
+    name?: string;
+}
+export declare class TSDI {
+    private components;
+    private instances;
+    private listener;
+    private properties;
+    addProperty(key: string, value: any): void;
+    close(): void;
+    enableComponentScanner(): void;
+    private registerComponent(componentMetadata);
+    register(component: Constructable<any>, name?: string): void;
+    private getComponentMetadataIndex(component, name?);
+    get<T>(component: Constructable<T>, hint?: string): T;
+}
+export declare function Component(options?: IComponentOptions): ClassDecorator;
+export declare function Inject(options?: IInjectOptions): PropertyDecorator;
+export declare function Initialize(): MethodDecorator;
