84485789d2372843840d7fa27c48ff5ad03fa3dc
David Schkalee <david.schkalee@sysvision.de>
2017-08-12 19:23:02 +0200

    refactor: extend constructor with static properties

--- lib/decorators.ts
+++ lib/decorators.ts
@@ -342,7 +342,10 @@ export function External(): ClassDecorator {
       return (target as any).__tsdi__.configureExternal(args, target);
     };
     (constructor as any).displayName = (target as any).name;
-    constructor.prototype = target.prototype;
+
+    Object.getOwnPropertyNames(target)
+      .filter(prop => prop !== '__tsdi__' && prop !== 'name' && prop !== 'length')
+      .forEach(prop => (constructor as any)[prop] = (target as any)[prop]);
     return constructor as any;
   };
 }
