1de783bde4238ebbe46de68bd519f4bbcaf1ea53
Markus Wolf <mail@markus-wolf.de>
2018-04-29 10:44:03 +0200

    feat: implement async component initializers
    Since components may require async initializers
    (e.g. to connect to some resource) the dependency
    tree could only be created async in some cases.
    To support this tsdi calls component initializers
    only if all injected component initializers are done.
    
    Closes #208

--- lib/initialize.ts
+++ lib/initialize.ts
@@ -7,6 +7,9 @@ export function Initialize(...args: any[]): MethodDecorator | void {
   const decorate = (target: Object, propertyKey: string | symbol) => {
     log('@Initialize %s#%s', (target.constructor as any).name, propertyKey);
     Reflect.defineMetadata('component:init', propertyKey, target);
+
+    const isAsync = Reflect.getMetadata('design:returntype', target, propertyKey) === Promise;
+    Reflect.defineMetadata('component:init:async', isAsync, target);
   };
   if (args.length > 0) {
     return decorate(args[0], args[1]);
