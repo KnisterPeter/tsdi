946f5b9bb25857ecdd399266d51a44467c2ef9cd
Frederick <frederick@gnodtke.net>
2018-04-29 13:20:26 +0200

    test: add test for deeply nested async dependencies

--- tests/index-test.ts
+++ tests/index-test.ts
@@ -215,6 +215,51 @@ describe('TSDI', () => {
       assert.isTrue(called);
     });
 
+    it('should call the initializer after multiple async dependencies are initialized', done => {
+      tsdi.enableComponentScanner();
+
+      @component
+      class DeepNestedAsyncDependency {
+        public value?: number;
+
+        @initialize
+        protected async init(): Promise<void> {
+          return new Promise<void>(resolve => {
+            setTimeout(() => {
+              this.value = 10;
+              resolve();
+            }, 10);
+          });
+        }
+      }
+
+      @component
+      class SyncDependency {
+        @inject public dependency!: DeepNestedAsyncDependency;
+        public value?: number;
+
+        @initialize
+        protected init(): void {
+          assert.equal(this.dependency.value, 10);
+          this.value = 10;
+        }
+      }
+
+      @component
+      class Dependent {
+        @inject public dependency!: SyncDependency;
+
+        @initialize
+        protected init(): void {
+          assert.equal(this.dependency.value, 10);
+          done();
+        }
+      }
+
+      // todo: this must throw because Dependent is async
+      tsdi.get(Dependent);
+    });
+
     it('should call the initalizer if all injections are itself initialized', done => {
       // this case test the case with async initializers
       tsdi.enableComponentScanner();
