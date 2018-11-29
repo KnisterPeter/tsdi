7d90b8c28723a134137a2ef563d975a3b596a6fe
Markus Wolf <mail@markus-wolf.de>
2018-11-29 10:49:14 +0100

    test: only generate abstact entry points

---
+++ tests/compiler/compiler.container-entries.test.ts
@@ -0,0 +1,31 @@
+import { runCompiler, testContainer } from './compiler.test.helper';
+
+test.only('TSDI compiler generates abstract entries and keeps concrete entries', async () => {
+  const files: { [name: string]: string } = {
+    '/file.ts': `
+      import { container, managed } from '/decorators';
+
+      @managed
+      export class Entry {
+      }
+
+      @container({ units: [] })
+      export abstract class Container {
+        public abstract entry: Entry;
+
+        public testEntry() {
+          // Notice: This is not a singleton!!!
+          return new Entry();
+        }
+      }
+
+      export function test(expect, container): void {
+        expect(container.entry).toEqual(container.testEntry());
+      }
+    `
+  };
+
+  await runCompiler(files);
+
+  await testContainer(files);
+});
