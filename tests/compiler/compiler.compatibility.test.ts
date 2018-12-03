bbd7afb51c5fe8ec3bf891ec4381644c63488d41
Markus Wolf <mail@markus-wolf.de>
2018-12-03 21:25:33 +0100

    refactor: split legacy tests

--- tests/compiler/compiler.compatibility.test.ts
+++ tests/compiler/compiler.compatibility.test.ts
@@ -6,30 +6,7 @@ test('TSDI compiler generates configuration for runtime components', async () =>
     'file.ts',
     `
       import { container } from 'tsdi/compiler/decorators';
-      import { TSDI, component, inject, initialize, destroy } from 'tsdi';
-
-      let initCalled = false;
-      let disposeCalled = false;
-
-      @component({scope: 'scope'})
-      export class ComponentWithScopeAndLifecycle {
-
-        @initialize
-        protected init(): void {
-          initCalled = true;
-          disposeCalled = true;
-        }
-
-        @destroy
-        protected dispose(): void {
-          disposeCalled = true;
-        }
-
-        public call(): void {
-          //
-        }
-
-      }
+      import { TSDI, component, inject } from 'tsdi';
 
       @component({singleton: false})
       export class NonSingleton {
@@ -51,23 +28,13 @@ test('TSDI compiler generates configuration for runtime components', async () =>
       export abstract class Container {
         public abstract component: Component;
         public abstract dependency: Dependency;
-        public abstract scoped: ComponentWithScopeAndLifecycle;
         public abstract nonSingleton: NonSingleton;
-
-        public getTSDI(): TSDI {
-          return this.tsdi;
-        }
       }
 
       export function test(expect, container) {
         expect(container.component).toBeInstanceOf(Component);
 
         expect(container.component.dependency).toBe(container.dependency);
-
-        expect(() => container.scoped.call()).toThrow();
-        container.getTSDI().getScope('scope').enter();
-        container.scoped.call()
-        container.getTSDI().getScope('scope').leave();
 
         expect(container.nonSingleton).not.toBe(container.nonSingleton);
       }
