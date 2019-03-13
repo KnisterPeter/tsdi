fcbfe0b1049aaefe90db2b5fd2f3f7050b9be824
Markus Wolf <mail@markus-wolf.de>
2019-03-13 20:28:40 +0100

    test: add test for invalid property injection

--- tests/compiler/injection-types/test.ts
+++ tests/compiler/injection-types/test.ts
@@ -10,3 +10,13 @@ test('Compiler should error on invalid constructor parameter types', () => {
       "But for [dependency] got 'type ID = number;' instead"
   );
 });
+
+test('Compiler should error on invalid property injection types', () => {
+  const compiler = new Compiler(join(__dirname, 'tsconfig.json'));
+  const container = compiler.getContainer<import('.').Container2>('Container2');
+
+  expect(() => container.generate(__dirname)).toThrow(
+    'Only interfaces or classes are valid types for property injection. ' +
+      "But for [dependency] got 'type ID = number;' instead"
+  );
+});
