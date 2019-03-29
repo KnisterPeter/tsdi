7a52e9a9c336ef5166e093e8a3c4c07482cd2f47
Markus Wolf <mail@markus-wolf.de>
2019-03-29 09:45:23 +0100

    test: make watcher test more robust

--- tests/compiler/webpack/test.ts
+++ tests/compiler/webpack/test.ts
@@ -49,7 +49,7 @@ afterEach(() => {
   }
 });
 
-test('TSDICompilerPlugin should run compiler right before compilation starts', done => {
+test('TSDICompilerPlugin should run compiler right before compilation starts', async done => {
   const compiler = webpack(config);
   compiler.run((err, stats) => {
     if (err || stats.hasErrors()) {
@@ -62,31 +62,41 @@ test('TSDICompilerPlugin should run compiler right before compilation starts', d
   });
 });
 
-test('TSDICompilerPlugin should rerun compiler in watch mode', done => {
-  let error: Error | undefined;
+test('TSDICompilerPlugin should rerun compiler in watch mode', async done => {
   let runs = 0;
 
   const compiler = webpack(config);
-  const watcher = compiler.watch({}, (err, stats) => {
-    runs++;
-    if (err || stats.hasErrors()) {
-      error = err || stats.compilation.errors[0];
-      return;
-    }
-  });
-  setTimeout(() => {
-    watcher.invalidate();
-
-    setTimeout(() => {
-      watcher.close(() => {
-        expect(error).toBeUndefined();
-        expect(existsSync(containerImplFile)).toBeTruthy();
-        expect(runs).toBe(2);
-
-        done();
+  let watcher: webpack.Compiler.Watching = undefined as any;
+  await new Promise(async (resolve2, reject) => {
+    // start compiler
+    await new Promise(resolve1 => {
+      watcher = compiler.watch({}, (err, stats) => {
+        runs++;
+        if (err || stats.hasErrors()) {
+          reject(err || stats.compilation.errors[0]);
+          return;
+        }
+        if (runs === 1) {
+          resolve1();
+        } else if (runs === 2) {
+          resolve2();
+        } else {
+          reject();
+        }
       });
-    }, 5000);
-  }, 5000);
+    });
+
+    // after first build => invalidate
+    watcher.invalidate();
+  });
+
+  // after second build => close and assert
+  watcher.close(() => {
+    expect(existsSync(containerImplFile)).toBeTruthy();
+    expect(runs).toBe(2);
+
+    done();
+  });
 });
 
 test('TSDICompilerPlugin should keep watch ignore list', done => {
