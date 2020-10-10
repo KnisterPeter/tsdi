b8ee0765995362b8f2eb37248e8f9fff349b2055
Markus Wolf <mail@markus-wolf.de>
2020-10-10 14:58:44 +0200

    chore: custom website publish

---
+++ website/publish.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra');
+const path = require('path');
+const shell = require('shelljs');
+
+const currentCommit = shell.exec('git rev-parse HEAD').stdout.trim();
+
+const siteConfig = require(`${process.cwd()}/siteConfig.js`);
+
+if (shell.exec(`git worktree add build/gh-pages gh-pages`).code !== 0) {
+  shell.echo('Error: git worktree failed');
+  shell.exit(1);
+}
+
+if (
+  shell.exec(`cp -r build/${siteConfig.projectName}/* build/gh-pages/`).code !==
+  0
+) {
+  shell.echo('Error: copy page failed');
+  shell.exit(1);
+}
+
+shell.exec('cd build/gh-pages');
+shell.exec('git add --all');
+if (
+  shell.exec(
+    `git commit -m "Update Page" -m "Deploy website version based on ${currentCommit}"`
+  ).code !== 0
+) {
+  shell.echo('Error: git commit failed');
+  shell.exit(1);
+}
+
+if (shell.exec(`git push origin gh-pages`).code !== 0) {
+  shell.echo('Error: Git push failed');
+  shell.exit(1);
+}
