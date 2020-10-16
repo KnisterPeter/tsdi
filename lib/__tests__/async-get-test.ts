e5c2e8611283cd349bd28093490ca34226495d16
Markus Wolf <mail@markus-wolf.de>
2020-10-16 08:58:59 +0200

    test: add asyncGet tests

---
+++ lib/__tests__/async-get-test.ts
@@ -0,0 +1,75 @@
+// tslint:disable: no-implicit-dependencies
+import { assert } from 'chai';
+import { TSDI, component, initialize, inject } from '..';
+
+describe('TSDI', () => {
+  let tsdi: TSDI;
+
+  beforeEach(() => {
+    tsdi = new TSDI();
+  });
+
+  afterEach(() => {
+    tsdi.close();
+  });
+
+  it('should return dependency when they are ready', async () => {
+    let runInit = false;
+
+    @component
+    class Component {
+      @initialize
+      protected init(): void {
+        runInit = true;
+      }
+    }
+    tsdi.register(Component);
+
+    await tsdi.asyncGet(Component);
+
+    assert.isTrue(runInit);
+  });
+
+  it('should return async dependency when they are ready', async () => {
+    let runInit = false;
+
+    @component
+    class Component {
+      @initialize
+      protected async init(): Promise<void> {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            runInit = true;
+            resolve();
+          }, 1000);
+        });
+      }
+    }
+    tsdi.register(Component);
+
+    await tsdi.asyncGet(Component);
+
+    assert.isTrue(runInit);
+  });
+
+  it('should return async dependency when they where previously ready', async () => {
+    let resolve: (() => void) | undefined;
+    const promise = new Promise((_resolve) => (resolve = _resolve));
+
+    @component
+    class Component {
+      @initialize
+      protected async init(): Promise<void> {
+        resolve?.();
+      }
+    }
+    tsdi.register(Component);
+
+    tsdi.get(Component);
+    await promise;
+
+    const comp = await tsdi.asyncGet(Component);
+
+    assert.instanceOf(comp, Component);
+  });
+});
